43673a0168cccd92eef4be55cc859ebb146b325f
benerees <benjamimrees@gmail.com>
2023-09-19 20:32:32 -0300

    feat: testes automatizados com jest e estrutura

--- bike/src/class/bike.ts
+++ bike/src/class/bike.ts
@@ -1,3 +1,5 @@
+import { Location } from "./location";
+
 export class Bike {
     constructor(
         public name: string,
@@ -8,8 +10,7 @@ export class Bike {
         public description: string,
         public ratings: number,
         public imageUrls: string[],
-        public latitude: number,
-        public longitude: number,
+        public location: Location = new Location(0.0, 0.0),
         public available: boolean = true,
         public id?: string
     ) {}
